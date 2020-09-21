d3f74f6ba34f38c50e7da2423236c3052db0b740
Richard J. Melito <45862086+DireWhale@users.noreply.github.com>
2020-09-20 19:30:34 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Yet_another_explanation_of_variance
+{
+    class BaseClass { }
+    class DerivedClass : BaseClass { }
+
+    delegate TOutput InvariantFunc<TOutput>();
+    delegate void InvariantAction<TInput>(TInput input);
+
+    class Program
+    {
+        DerivedClass ReturnDerivedClass()
+        {
+            return new DerivedClass();
+        }
+
+        void ArgumentBaseClass(BaseClass input)
+        {
+
+        }
+
+        void LegalInvariance()
+        {
+            InvariantFunc<BaseClass> notCovariance = ReturnDerivedClass;
+
+            InvariantAction<DerivedClass> notContravariance = ArgumentBaseClass;
+        }
+
+        void IllegalInvariance()
+        {
+            InvariantFunc<DerivedClass> returnDerived = ReturnDerivedClass;
+            // illegal:
+            InvariantFunc<BaseClass> needsCovariance = returnDerived;
+
+
+            InvariantAction<BaseClass> argumentBase = ArgumentBaseClass;
+            // illegal:
+            InvariantAction<DerivedClass> needsContravariance = argumentBase;
+        }
+
+        void Covariance()
+        {
+            Func<DerivedClass> returnDerived = ReturnDerivedClass;
+            // legal:
+            Func<BaseClass> usesCovariance = returnDerived;
+        }
+
+        void Contravariance()
+        {
+            Action<BaseClass> argumentBase = ArgumentBaseClass;
+            // legal:
+            Action<DerivedClass> usesContravariance = argumentBase;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
